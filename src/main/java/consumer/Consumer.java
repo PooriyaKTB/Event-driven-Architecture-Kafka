ee9689d640f29fa3e239e82a142da83975e7cc93
Pooriya <pooriya.ketabi@gmail.com>
2026-09-08 17:35:54 +0100

    Producer & Consumer initial implementation

---
+++ src/main/java/consumer/Consumer.java
@@ -0,0 +1,48 @@
+package consumer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Consumer {
+    public static void main(String[] args) {
+
+        Logger log = LoggerFactory.getLogger(Consumer.class);
+
+        try (KafkaConsumer<String, String> consumer = getConsumer()) {
+
+            String topic = "cron-jobs";
+
+            consumer.subscribe(Collections.singleton(topic));
+
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+                for (ConsumerRecord<String, String> record : records) {
+//                    log.info(String.valueOf(record));
+                    log.info("Received -> Key: {}, Partition: {}, Offset: {}, Value: {}", record.key(), record.partition(), record.offset(), record.value());
+                }
+            }
+        }
+    }
+
+    public static KafkaConsumer<String, String> getConsumer() {
+
+        Properties config = new Properties();
+
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "cron-app");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        return new KafkaConsumer<>(config);
+    }
+}
---
+++ src/main/java/producer/Producer.java
@@ -0,0 +1,78 @@
+package producer;
+
+import java.util.Properties;
+import java.util.UUID;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Producer {
+    public static void main(String[] args) {
+
+        final Logger log = LoggerFactory.getLogger(Producer.class);
+
+        KafkaProducer<String, String> producer = getProducer();
+
+//         Sample message
+        String topic = "cron-jobs";
+        String key = UUID.randomUUID().toString();
+        String messagePayload = "{\"message\": \"Hello World from Pooriya\"}";
+
+        final ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, messagePayload);
+
+//        Send to Kafka
+//        producer.send(record);
+        producer.send(record, (metadata, exception) -> {
+            if (exception == null) {
+                log.info("Sent! Partition: {}, Offset: {}", metadata.partition(), metadata.offset());
+            } else {
+                log.error("Error sending message: " + exception.getMessage());
+            }
+        });
+
+//        Close the connection when it's done
+        producer.close();
+        log.info("Message sent successfully!");
+    }
+
+    private static KafkaProducer<String, String> getProducer() {
+
+        Properties config = new Properties();
+
+//        Where is kafka? on local host port 9092, for initial connection (Initial Contact Point) and getting Cluster metadata
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+
+//        Serialising data
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+//        Create producer base on config
+        KafkaProducer<String, String> producer = new KafkaProducer<>(config);
+        return producer;
+    }
+}
+    /*
+        Properties config = new Properties();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(config);
+
+        String topic = "cron-jobs";
+        String key = UUID.randomUUID().toString();
+        String messagePayload = "{\"jobId\": 1, \"command\": \"echo Hello\"}";
+
+        ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, messagePayload);
+
+        producer.send(record);
+
+        producer.close();
+        System.out.println("Message sent successfully!");
+    */
